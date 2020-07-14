9174ee188bc94a8d55f678e7e50df23248488925
erupakasaiakshay <erupakasaiakshay@gmail.com>
2020-07-14 17:06:40 -0400

    added Viewholder to recycleview custom adapter

--- app/src/main/java/com/example/weather_app/CustomAdapter.java
+++ app/src/main/java/com/example/weather_app/CustomAdapter.java
@@ -1,10 +1,16 @@
 package com.example.weather_app;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.weatherapp.R;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder>{
 
@@ -23,5 +29,25 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView dayweather_icon;
+        TextView dayname,weather;
+        LinearLayout list_itemm;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            dayweather_icon = itemView.findViewById(R.id.dayweather_icon);
+            dayname = itemView.findViewById(R.id.day_name);
+            weather = itemView.findViewById(R.id.weather);
+            list_itemm = itemView.findViewById(R.id.list_itemm);
+
+
+            itemView.setTag(this);
+
+        }
     }
 }
