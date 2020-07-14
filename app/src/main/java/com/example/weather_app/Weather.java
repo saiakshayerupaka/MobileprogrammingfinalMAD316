5b3157d03faf65366f4538211ce7083a3c2692fc
deepmal933 <deepmal944@gmail.com>
2020-07-14 11:19:24 -0400

    fixed model class

--- app/src/main/java/com/example/weather_app/Weather.java
+++ app/src/main/java/com/example/weather_app/Weather.java
@@ -97,7 +97,17 @@ public class Weather implements Serializable {
         this.weather_state_abbr = weather_state_abbr;
     }
 
-
+    public Weather(String weather_state_name, double min_temp, double max_temp, double wind_speed, int humidity, int predictability, String weather_state_abbr, String applicable_date, double the_temp) {
+        this.weather_state_name = weather_state_name;
+        this.min_temp = min_temp;
+        this.max_temp = max_temp;
+        this.wind_speed = wind_speed;
+        this.humidity = humidity;
+        this.predictability = predictability;
+        this.weather_state_abbr = weather_state_abbr;
+        this.applicable_date = applicable_date;
+        this.the_temp = the_temp;
+    }
 
     public double getThe_temp() {
         return the_temp;
