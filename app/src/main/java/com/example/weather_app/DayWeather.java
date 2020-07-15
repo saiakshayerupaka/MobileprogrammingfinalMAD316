9c5bef5ee34626c396f1172f64f53cbc887f8f28
deepmal933 <deepmal944@gmail.com>
2020-07-14 20:14:47 -0400

    fixed and updated day weather activity

--- app/src/main/java/com/example/weather_app/DayWeather.java
+++ app/src/main/java/com/example/weather_app/DayWeather.java
@@ -1,14 +1,22 @@
 package com.example.weather_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.example.weatherapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.weatherapp.R;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DayWeather extends AppCompatActivity {
 
@@ -20,6 +28,12 @@ public class DayWeather extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_weather);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Weather we = (Weather) getIntent().getSerializableExtra("weather");
+
+        Toast.makeText(DayWeather.this, "" + we.getWeather_state_name(), Toast.LENGTH_LONG).show();
 
         weather_image = findViewById(R.id.weather_icon);
         city = findViewById(R.id.city);
@@ -30,6 +44,31 @@ public class DayWeather extends AppCompatActivity {
         prediction = findViewById(R.id.prediction);
         weather = findViewById(R.id.weather);
 
+        Glide.with(this)
+                .load("https://www.metaweather.com/static/img/weather/png/" + we.getWeather_state_abbr() + ".png")
+                .into(weather_image);
+
+        weather.setText(we.getWeather_state_name());
+        min_temp.setText(String.format("%.2f", we.getMin_temp()));
+
+        max_temp.setText(String.format("%.2f", we.getThe_temp()));
+        wind_speed.setText(String.format("%.2f", we.getMax_temp()));
+
+        prediction.setText(String.valueOf(we.getPredictability()) + "%");
+        humidity.setText(String.valueOf(we.getHumidity()));
+
+
+        SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-mm-DD");
+        Date date = null;
+        try {
+            date = inFormat.parse(we.getApplicable_date());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat outFormat = new SimpleDateFormat("EEEE");
+        String goal = outFormat.format(date);
+
+        city.setText(goal);
 
         close = findViewById(R.id.close);
 
@@ -39,5 +78,10 @@ public class DayWeather extends AppCompatActivity {
                 finish();
             }
         });
+
+
+
+
     }
+
 }
